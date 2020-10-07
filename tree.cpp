4a3119ed40c22c5d2c94d7cb37fd1b9149e4df56
Fedor Skvortsov <43882212+fedorskvorcov@users.noreply.github.com>
2020-10-07 12:04:54 +0300

    Create tree.cpp

---
+++ tree.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <queue>
+#include <malloc.h>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+};
+
+void inOrder(struct node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+void preOrder(struct node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+void postOrder(struct node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+void levelOrder(struct node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    queue<node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        struct node *current = q.front();
+        cout << current->data << " ";
+        if (current->left != NULL)
+        {
+            q.push(current->left);
+        }
+        if (current->right != NULL)
+        {
+            q.push(current->right);
+        }
+        q.pop();
+    }
+}
+
+struct node *newNode(int data)
+{
+    struct node *node = (struct node *)malloc(sizeof(struct node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+
+    return node;
+}
+
+int main()
+{
+    struct node *root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    root->right->left = newNode(6);
+    root->right->right = newNode(7);
+
+    cout << "LEVEL order traversal of binary tree is" << endl;
+    levelOrder(root);
+    cout << endl;
+    cout << "IN order traversal of binary tree is" << endl;
+    inOrder(root);
+    cout << endl;
+    cout << "PRE order traversal of binary tree is" << endl;
+    preOrder(root);
+    cout << endl;
+    cout << "POST order traversal of binary tree is" << endl;
+    postOrder(root);
+
+    return 0;
+}
